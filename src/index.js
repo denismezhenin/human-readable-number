cf0dbd01e5d3f8394c1637f7e18a497101cac4c6
denismezhenin <dzianismiazhenin@gmail.com>
2022-08-05 15:17:36 +0300

    new3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function toReadable (number) {
   let numbers = ([['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'], 
   ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'], 
   ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']])
-  let num = number.toString()
+  let num = number.toString();
   if (num.length == 1) {
     return `${numbers[0][num]}`
   }
